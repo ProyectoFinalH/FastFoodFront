2a8de0b8e1e303a824a1ce192738f3c377bd9065
CunGemcrak <142614397+CunGemcrak@users.noreply.github.com>
2024-05-28 23:47:22 -0500

    Cambio del gemcrak
    verificar problemas del push

--- src/Components/loading/Loading.js
+++ src/Components/loading/Loading.js
@@ -3,7 +3,7 @@ import loadingimg from "./Loadin_imagenes/iconos/giphy.gif";
 const Loading = () => {
   return (
     <div className="bodyloading">
-      <img src={loadingimg} alt="Cargandopppp" />
+      <img src={loadingimg} alt="CargandoppppGemcrak" />
     </div>
   );
 };
