001e59a7180469f44e85064060edffa9c9654544
Daniel Navarro <danielkristopher0563@gmail.com>
2024-05-29 14:15:44 -0500

    cambios daniel
    mejora css y al iniciar la cuenta

--- src/Components/Login/Login_Ingreso/Login_Ingreso.js
+++ src/Components/Login/Login_Ingreso/Login_Ingreso.js
@@ -1,92 +1,118 @@
-import './Login_ingreso.css'
-import {useState} from 'react'
+import React, { useState } from "react";
+import "./Login_ingreso.css";
+import imagen from "../Login_imagenes/food_sin_fondosi.png";
+import icono_usuario from "../Login_imagenes/iconos/usuario.png";
+import icono_key from "../Login_imagenes/iconos/contrasena.png";
+import icono_ver from "../Login_imagenes/iconos/cerrar-ojo-black.png";
+import icono_google from "../Login_imagenes/iconos/icons8-google-48.png";
+import icono_ocultar from "../Login_imagenes/iconos/ojo-con-pestanas-black.png";
+import validationIngreso from "./Validar_Login_ingreso";
 
-
-
-import imagen from '../Login_imagenes/food_sin_fondosi.png'
-import icono_usuario from '../Login_imagenes/iconos/usuario.png'
-import icono_key from '../Login_imagenes/iconos/contrasena.png'
-import icono_ver from '../Login_imagenes/iconos/cerrar-ojo-black.png'
-import icono_google from '../Login_imagenes/iconos/icons8-google-48.png'
-import icono_ocultar from '../Login_imagenes/iconos/ojo-con-pestanas-black.png'
-
-import validationIngreso from './Validar_Login_ingreso'
 const LoginIngreso = ({ setView }) => {
-    const [keyver,setKeyver] = useState(false);
-    const [errors, setErrors] = useState({});
-    const [userData, setUserData]=useState({
-        name:'',
-        pass:'',
+  const [keyver, setKeyver] = useState(false);
+  const [userData, setUserData] = useState({
+    name: "",
+    pass: "",
+  });
+  const [errors, setErrors] = useState({});
+  const [attemptedSubmit, setAttemptedSubmit] = useState(false);
+
+  const handlever = () => {
+    setKeyver(!keyver);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserData({
+      ...userData,
+      [name]: value,
     });
-    const handlever = () =>{
-        //  alert("ingreso");
-     
-              setKeyver(!keyver)
-         
-          
-      }
+  };
 
-    const handlChange = (event) =>{
-       
-        //  const { name, value } = event.target;
-       
-          setUserData({
-              ...userData,
-              [event.target.name]:event.target.value
-          })
-          setErrors(validationIngreso({
-              ...userData,
-              [event.target.name]:event.target.value
-          }))
-        
-  
-      }
-    return (
-        <div className="bodyIngreso">
-        <img src={imagen} alt='Logo Fast Foot' className='imageningreso'/> 
-        <div className="contenedoringreso">
-            <div className="Grupoingreso">
-                <div className="centrarlogogoogle">
-                <img src={icono_google} alt="icono ingreso"/><div> ingresar con google</div>
-                </div>
-            </div>
-            <div className="Grupoinput">
-                <img src={icono_usuario} alt="icono ingreso"/>
-                <input type="text"
-                name="name" 
-                value={userData.name}
-                onChange={handlChange}
-                maxLength={100}
-                placeholder="Celular/Correo"
-                title='No Puede ser mayor a 100 caracteres'/>
-            </div>
-            <div className="Grupoinput">
-                <img src={icono_key} alt="icono ingreso"/>
-                <input type={!keyver ? "password" : "text"}
-                 name="pass" 
-                 value={userData.pass}
-                 onChange={handlChange}
-                 maxLength={15}
-                 placeholder="Password"
-                 title='No Puede ser mayor a 15 caracteres'/>
-                <img src={keyver ? icono_ocultar : icono_ver}alt='Mostrar/Ocultar'  onClick={handlever} className='ver'/>
-            </div>
-            <div className="Registrate" onClick={()=>{setView('recuperarkey')}}>¿Olvidaste tu Contraseña?</div>
-            <div className="espacioError">
-                {
-                    //aqui van los errores 
-                    errors.name
-                }
-            </div>
-            <div className="ov-btn-grow-box ">Ingresar</div>
-            <div className="Registrate .olvidastekey" onClick={()=>{setView('registro')}}>Registrarse</div>
-            <div className="seleccion">o</div>
-            <div className="Registrate"  onClick={()=>{setView('invitado')}}>Ingresa como Invitado</div>
-        </div>
-        </div>
-        ) 
-        
+  const handleSubmit = () => {
+    const validationErrors = validationIngreso(userData);
+    setErrors(validationErrors);
+    setAttemptedSubmit(true);
 
+    if (Object.keys(validationErrors).length === 0) {
     }
+  };
 
-export default LoginIngreso
+  return (
+    <div className="bodyIngreso">
+      <img src={imagen} alt="Logo Fast Foot" className="imageningreso" />
+      <div className="contenedoringreso">
+        <div className="Grupoinput">
+          <img src={icono_usuario} alt="icono ingreso" />
+          <input
+            type="text"
+            name="name"
+            value={userData.name}
+            onChange={handleChange}
+            maxLength={100}
+            placeholder="Celular/Correo"
+          />
+          {attemptedSubmit && errors.name && (
+            <div className="espacioError">{errors.name}</div>
+          )}
+        </div>
+        <div className="Grupoinput">
+          <img src={icono_key} alt="icono ingreso" />
+          <input
+            type={!keyver ? "password" : "text"}
+            name="pass"
+            value={userData.pass}
+            onChange={handleChange}
+            maxLength={15}
+            placeholder="Password"
+          />
+          <img
+            src={keyver ? icono_ocultar : icono_ver}
+            alt="Mostrar/Ocultar"
+            onClick={handlever}
+            className="ver"
+          />
+          {attemptedSubmit && errors.pass && (
+            <div className="espacioError">{errors.pass}</div>
+          )}
+        </div>
+
+        <div
+          className="olvidastekey"
+          onClick={() => {
+            setView("recuperarkey");
+          }}
+        >
+          ¿Olvidaste tu Contraseña?
+        </div>
+
+        <div className="ov-btn-container">
+          <div className="ov-btn-grow-box" onClick={handleSubmit}>
+            Ingresar
+          </div>
+        </div>
+
+        <div className="Grupoingreso">
+          <div className="centrarlogogoogle">
+            <img src={icono_google} alt="icono ingreso" />
+            <div> ingresar con google</div>
+          </div>
+          <div
+            className="Registrate"
+            onClick={() => {
+              setView("invitado");
+            }}
+          >
+            Ingresa como Invitado
+          </div>
+
+          <div className="Registrate">
+            ¿No tienes una cuenta? Regístrate aquí
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginIngreso;
--- src/Components/Login/Login_Principal/Login_Principal.js
+++ src/Components/Login/Login_Principal/Login_Principal.js
@@ -1,43 +1,36 @@
-import './Login_Principal.css';
-import Carrucel from '../Carrucel/Carrucel';
-import LoginIngreso from '../Login_Ingreso/Login_Ingreso';
-import Recuperarkey from '../Login_Recuperar/Login_Recuperar_key';
-import { useEffect, useState } from 'react';
-import Loading from '../../loading/Loading';
+import "./Login_Principal.css";
+import Carrucel from "../Carrucel/Carrucel";
+import LoginIngreso from "../Login_Ingreso/Login_Ingreso";
+import Recuperarkey from "../Login_Recuperar/Login_Recuperar_key";
+import { useEffect, useState } from "react";
+import Loading from "../../loading/Loading";
 
 const LoginPrincipal = () => {
-    const [logueo, setLogueo] = useState('login');
-    const [loading, setLoading] = useState(true);
+  const [logueo, setLogueo] = useState("login");
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 10000); // 5 segundos en milisegundos
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
 
-        return () => clearTimeout(timer); // Limpiar el temporizador cuando el componente se desmonte
-    }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-    return (
-        <div>
-            {loading && <Loading />}
-        <div className='BodyLogin'>
-              
-            <div className='izquierdaGeneral'>
-                
-                   <Carrucel className="carrucel"/>
-                    
-            </div>
-            <div className='derechaGeneral'>
-                  
-                    {logueo === 'login' && <LoginIngreso setView={setLogueo} />}
-                    {logueo === 'recuperarkey' && <Recuperarkey setView={setLogueo} />}
-                    
-                  
-                    
-            </div>
+  return (
+    <div>
+      {loading && <Loading />}
+      <div className="BodyLogin">
+        <div className="izquierdaGeneral">
+          <Carrucel className="carrucel" />
         </div>
+        <div className="derechaGeneral">
+          {logueo === "login" && <LoginIngreso setView={setLogueo} />}
+          {logueo === "recuperarkey" && <Recuperarkey setView={setLogueo} />}
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default LoginPrincipal;
