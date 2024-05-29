b97a8ff976dfe39e8ad5570a91925dd26df3f2a5
Daniel Navarro <danielkristopher0563@gmail.com>
2024-05-28 23:31:04 -0500

    Daniel prueba

--- src/Components/loading/Loading.js
+++ src/Components/loading/Loading.js
@@ -1,12 +1,10 @@
-import './Loading.css'
-import loadingimg from './Loadin_imagenes/iconos/giphy.gif'
-const Loading = ()=> {
-
-    return(
+import "./Loading.css";
+import loadingimg from "./Loadin_imagenes/iconos/giphy.gif";
+const Loading = () => {
+  return (
     <div className="bodyloading">
-        <img src={loadingimg} alt="Cargando"/>
+      <img src={loadingimg} alt="Cargandopppp" />
     </div>
-            
-    )
-}
+  );
+};
 export default Loading;
