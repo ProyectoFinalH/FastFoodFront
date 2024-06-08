a51f3bba54df5c21cfa67618ee4607ed04e11801
Daniel Navarro <danielkristopher0563@gmail.com>
2024-06-08 17:18:19 -0500

    Actualización css

--- src/views/account/account.js
+++ src/views/account/account.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import Navbar from "../../Components/navbar/navbar";
@@ -9,11 +9,21 @@ function Account() {
   const user = useSelector((state) => state.USER);
   const dispatch = useDispatch();
 
-  const [firstName, setFirstName] = useState(user?.firstName || "");
-  const [lastName, setLastName] = useState(user?.lastName || "");
-  const [gender, setGender] = useState(user?.gender || "");
-  const [birthDate, setBirthDate] = useState(user?.birthDate || "");
-  const [profileImage, setProfileImage] = useState(user?.profileImage || "");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [gender, setGender] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+
+  useEffect(() => {
+    if (user) {
+      setFirstName(user.firstName || "");
+      setLastName(user.lastName || "");
+      setGender(user.gender || "");
+      setBirthDate(user.birthDate || "");
+      setProfileImage(user.profileImage || "");
+    }
+  }, [user]);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -27,6 +37,11 @@ function Account() {
   };
 
   const handleSubmit = async () => {
+    if (!firstName || !lastName || !gender || !birthDate) {
+      alert("Por favor, completa todos los campos.");
+      return;
+    }
+
     const updatedUser = {
       ...user,
       firstName,
@@ -35,6 +50,7 @@ function Account() {
       birthDate,
       profileImage,
     };
+
     dispatch(updateUser(updatedUser));
 
     try {
@@ -67,66 +83,117 @@ function Account() {
     <div>
       <Navbar />
       <div className="account-container">
-        <h2>Información de tu cuenta</h2>
-        <div className="account-info">
-          <div className="profile-image-container">
-            {profileImage ? (
-              <img src={profileImage} alt="Profile" className="profile-image" />
-            ) : (
-              <div className="profile-placeholder">Imagen de perfil</div>
-            )}
-            <label className="profile-image-label">
-              Cambiar foto
+        <div className="account-sidebar">
+          <div className="profile-header">
+            <div
+              className="profile-image-container"
+              onClick={() =>
+                document.getElementById("profileImageInput").click()
+              }
+            >
+              {profileImage ? (
+                <img
+                  src={profileImage}
+                  alt="Profile"
+                  className="profile-image"
+                />
+              ) : (
+                <div className="profile-placeholder">Imagen de perfil</div>
+              )}
               <input
                 type="file"
+                id="profileImageInput"
                 accept="image/*"
                 onChange={handleImageChange}
+                style={{ display: "none" }}
               />
-            </label>
+            </div>
+            <h2>{user?.firstName}</h2>
+            <p>Mi perfil</p>
           </div>
-          <div className="input-group">
-            <label>Correo:</label>
-            <input type="email" value={user?.email} disabled />
-          </div>
-          <div className="input-group">
-            <label>Nombre:</label>
-            <input
-              type="text"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </div>
-          <div className="input-group">
-            <label>Apellido:</label>
-            <input
-              type="text"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </div>
-          <div className="input-group">
-            <label>Género:</label>
-            <select value={gender} onChange={(e) => setGender(e.target.value)}>
-              <option value="">Seleccione</option>
-              <option value="hombre">Hombre</option>
-              <option value="mujer">Mujer</option>
-            </select>
-          </div>
-          <div className="input-group">
-            <label>Fecha de nacimiento:</label>
-            <input
-              type="date"
-              value={birthDate}
-              onChange={(e) => setBirthDate(e.target.value)}
-            />
-          </div>
-          <button onClick={handleSubmit} className="update-button">
-            Actualizar datos
-          </button>
+          <nav className="menu">
+            <ul>
+              <li>
+                <Link to="#">Ajustes de cuenta</Link>
+              </li>
+              <li>
+                <Link to="#">Pagos</Link>
+              </li>
+              <li>
+                <Link to="#">Centro de notificaciones</Link>
+              </li>
+              <li>
+                <Link to="#">Últimas órdenes</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <Link to="/home" className="home-button">
-          Volver al inicio
-        </Link>
+        <div className="account-info">
+          <h2>Información de tu cuenta</h2>
+          <div className="input-group-container">
+            <div className="input-group1">
+              <label>Nombre(s)</label>
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+            <div className="input-group1">
+              <label>Apellido(s)</label>
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="input-group-container">
+            <div className="input-group1">
+              <label>Correo Electrónico</label>
+              <input type="email" value={user?.email} disabled />
+            </div>
+            <div className="input-group1">
+              <label>Fecha de nacimiento</label>
+              <input
+                type="date"
+                value={birthDate}
+                onChange={(e) => setBirthDate(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="input-group-gender">
+            <label>Género</label>
+            <div className="gender-options">
+              <label>
+                <input
+                  type="radio"
+                  value="Hombre"
+                  checked={gender === "Hombre"}
+                  onChange={(e) => setGender(e.target.value)}
+                />
+                Hombre
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="Mujer"
+                  checked={gender === "Mujer"}
+                  onChange={(e) => setGender(e.target.value)}
+                />
+                Mujer
+              </label>
+            </div>
+          </div>
+          <div className="button-group">
+            <button onClick={handleSubmit} className="update-button">
+              Actualizar datos
+            </button>
+            <Link to="/" className="home-button">
+              Volver al inicio
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
