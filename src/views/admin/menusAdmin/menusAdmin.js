631418dd7ba06a60d5e1e11a1a069d73c2ae4df6
Jdcisneros <jorge_08.8@hotmail.com>
2024-06-16 21:44:58 -0300

    actualizacion errores

--- src/views/admin/menusAdmin/menusAdmin.js
+++ src/views/admin/menusAdmin/menusAdmin.js
@@ -73,21 +73,21 @@ function MenusAdmin({ allMenusAdmin, allMenuItemsAdmin, allRestaurantsAdmin }) {
                 .map((menu) => (
                   <div
                     key={menu.id}
-                    onClick={() => handleMenuSelect(restaurant.id, menu.id)}
-                    className={`MenusAdmin ${menu.active ? '' : 'inactive'} ${
+                    onClick={() => handleMenuSelect(restaurant?.id, menu?.id)}
+                    className={`MenusAdmin ${menu?.active ? '' : 'inactive'} ${
                       selectedMenuIds[restaurant.id] === menu.id ? 'selectedMenu' : ''
                     }`}
                   >
                     <div className="menuSelectAdmin">
                       <ul>
                         <div className="menuLi">
-                          <li>{menu.name}</li>
+                          <li>{menu?.name}</li>
                         </div>
                       </ul>
                       <button
                         className="buttonactdesMenus"
                         onClick={() =>
-                          toggleActivationMenu(menu.id, !menu.active)
+                          toggleActivationMenu(menu?.id, !menu?.active)
                         }
                       >
                         {menu.active ? (
@@ -101,11 +101,11 @@ function MenusAdmin({ allMenusAdmin, allMenuItemsAdmin, allRestaurantsAdmin }) {
                 ))}
             </div>
             <div className="MenuItemsContainer">
-              {selectedMenuIds[restaurant.id] && // Verifica si hay un menu seleccionado para este rest
+              {selectedMenuIds[restaurant?.id] && // Verifica si hay un menu seleccionado para este rest
                 menuItems
                   .filter(
                     (menuItem) =>
-                      menuItem?.menu_id === selectedMenuIds[restaurant.id]
+                      menuItem?.menu_id === selectedMenuIds[restaurant?.id]
                   )
                   .map((menuItem) => (
                     <div
@@ -115,30 +115,30 @@ function MenusAdmin({ allMenusAdmin, allMenuItemsAdmin, allRestaurantsAdmin }) {
                       }`}
                     >
                       <div className="imageItem">
-                        <img src={menuItem.image_url} alt="imgItem" />
+                        <img src={menuItem?.image_url} alt="imgItem" />
                       </div>
                       <div className="nameItem">
                         <div>
-                          <h2>{menuItem.name}</h2>
+                          <h2>{menuItem?.name}</h2>
                         </div>
                         <div>
-                          <p title={menuItem.description}>
-                            {menuItem.description.substring(0, 25)}...
+                          <p title={menuItem?.description}>
+                            {menuItem?.description?.substring(0, 25)}...
                           </p>
                         </div>
                       </div>
-                      <div className="priceItem">${menuItem.price}</div>
+                      <div className="priceItem">${menuItem?.price}</div>
       
                         <button
                           className="buttonactdesMenus"
                           onClick={() =>
                             toggleActivationMenuItem(
-                              menuItem.id,
-                              !menuItem.active
+                              menuItem?.id,
+                              !menuItem?.active
                             )
                           }
                         >
-                          {menuItem.active ? (
+                          {menuItem?.active ? (
                             <img src={activar} alt="activar" />
                           ) : (
                             <img src={desactivar} alt="desactivar" />
--- src/views/admin/ordenesAdmin/ordenesAdmin.js
+++ src/views/admin/ordenesAdmin/ordenesAdmin.js
@@ -34,83 +34,40 @@ console.log("orden:",orders)
         </thead>
         <tbody>
           {orders?.map((order) => (
-            <tr key={order.id}>
-              <td>{order.id}</td>
-              <td>{order.user_name}</td>
-              <td>{order.restaurant_name}</td>
+            <tr key={order?.id}>
+              <td>{order?.id}</td>
+              <td>{order?.user_name}</td>
+              <td>{order?.restaurant_name}</td>
               <td>
-                {order.items.map((item, index) => (
+                {order?.items?.map((item, index) => (
                   <div key={index}>
-                    <p>{item.name_item}</p>
+                    <p>{item?.name_item}</p>
                   </div>
                 ))}
               </td>
               <td>
-                {order.items.map((item, index) => (
+                {order?.items?.map((item, index) => (
                   <div key={index}>
-                    <p>{item.quantity}</p>
+                    <p>{item?.quantity}</p>
                   </div>
                 ))}
               </td>
               <td>
-                {order.items.map((item, index) => (
+                {order?.items?.map((item, index) => (
                   <div key={index}>
-                    <p>{item.partial_price}</p>
+                    <p>{item?.partial_price}</p>
                   </div>
                 ))}
               </td>
-              <td>Dia: {order.order_date.slice(0, 19).replace("T"," Hora: ")}</td>
-              <td>{order.status_order}</td>
-              <td>{order.total_price}</td>
+              <td>Dia: {order?.order_date?.slice(0, 19).replace("T"," Hora: ")}</td>
+              <td>{order?.status_order}</td>
+              <td>{order?.total_price}</td>
             </tr>
           ))}
         </tbody>
       </table>
         </div>
-       {/* {orders?.map((order) => (
-         <div key={order.id} className="OrderAdminContainer">
-             <div >
-               <h3>Usuario:</h3>
-               <p>{order.user_name}</p>
-             </div>
-             <div>
-               <h3>Restaurante:</h3>
-               <p>{order.restaurant_name}</p>
-             </div>
-             <div>
-               <h3>Items:</h3>
-               {order.items.map((item, index) => (
-                 <div key={index}>
-                   <div>
-                     <h3>Nombre del Producto:</h3>
-                     <p>{item.name_item}</p>
-                   </div>
-                   <div>
-                     <h3>Cantidad:</h3>
-                     <p>{item.quantity}</p>
-                   </div>
-                   <div>
-                     <h3>Precio:</h3>
-                     <p>{item.partial_price}</p>
-                   </div>
-                 </div>
-               ))}
-             </div>
-             <div>
-               <h3>Hora de compra:</h3>
-               <p>{order.order_date}</p>
-             </div>
-             <div>
-               <h3>Estado de compra:</h3>
-               <p>{order.status_order}</p>
-             </div>
-             <div>
-               <h3>Total:</h3>
-               <p>{order.total_price}</p>
-             </div>
-           </div>
-        
-       ))} */}
+       
       </div>
      </div>
   )
