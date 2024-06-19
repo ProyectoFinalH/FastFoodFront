f4cfbb4b0c8e57b9ff793fff8aad9499af09d871
Jdcisneros <jorge_08.8@hotmail.com>
2024-06-18 23:57:05 -0300

    admin view

--- src/views/admin/admin.js
+++ src/views/admin/admin.js
@@ -1,15 +1,39 @@
 import Sidebar from "./sidebar/sidebar";
 // import logo from "../../images/logo.png"
 import "./admin.css";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Loading from "../../Components/loading/Loading";
 
 
 function Admin() {
 
-  
+  const USER = useSelector((state)=> state.USER)
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
+  useEffect(()=>{
+    if (USER && USER.role_id === 2){
+      // alertify.alert("Mensaje:", "Hola administrador")
+
+    }else{
+      navigate("/loginAdmin")
+    }
+  })
 
   return (
     <div className="adminContainer">
-      
+      {loading && <Loading />}
         <Sidebar />
       
     </div>
