3f2e21f714cec38001a9b939d9d236a15dcfbe7a
CunGemcrak <142614397+CunGemcrak@users.noreply.github.com>
2024-06-18 21:04:52 -0500

    Solo es un segundo commit

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -115,6 +115,8 @@ function Detail({ isOpen, handleCloseModal, menuItemId }) {
                 alt="Carrito"
                 className="aumentardisminuir"
                 onClick={handleMenuCarrito}
+
+                
               />
             </div>
           </div>
