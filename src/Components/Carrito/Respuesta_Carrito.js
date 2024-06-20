0e956c2ad656883af09aad2c949902fec0e2aa91
CunGemcrak <142614397+CunGemcrak@users.noreply.github.com>
2024-06-20 18:36:55 -0500

    Mercado pago funcionado

--- src/Components/Carrito/Respuesta_Carrito.js
+++ src/Components/Carrito/Respuesta_Carrito.js
@@ -1,57 +1,35 @@
-import React, { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import alertify from 'alertifyjs';
+import 'alertifyjs/build/css/alertify.css'; // Importa los estilos de alertify
+import 'alertifyjs/build/css/themes/default.css'; 
+
+
+import Navbar from '../navbar/navbar';
 import Loading from '../loading/Loading';
+import { useEffect, useState } from 'react';
 
 const RespuestaCarr = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      const searchParams = new URLSearchParams(location.search);
-  
-      const collectionId = searchParams.get('collection_id');
-      const collectionStatus = searchParams.get('collection_status');
-      const paymentId = searchParams.get('payment_id');
-      const status = searchParams.get('status');
-      const externalReference = searchParams.get('external_reference');
-      const paymentType = searchParams.get('payment_type');
-      const merchantOrderId = searchParams.get('merchant_order_id');
-      const preferenceId = searchParams.get('preference_id');
-      const siteId = searchParams.get('site_id');
-      const processingMode = searchParams.get('processing_mode');
-      const merchantAccountId = searchParams.get('merchant_account_id');
-  
-      // Aquí puedes manejar la respuesta y realizar cualquier acción necesaria
-      console.log({
-        collectionId,
-        collectionStatus,
-        paymentId,
-        status,
-        externalReference,
-        paymentType,
-        merchantOrderId,
-        preferenceId,
-        siteId,
-        processingMode,
-        merchantAccountId,
-      });
-  
-      // Ejemplo de redirección después de procesar la información
-      if (status === 'approved') {
-        alert('Pago aprobado. Redirigiendo al menú...');
-        navigate('/menu');
-      } else {
-        alert('Pago no aprobado. Por favor, verifica tu información.');
-        navigate('/menu');
-      }
-    }, [location, navigate]);
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   
     return (
-      <div>
-        <Loading />
-        <p>Procesando respuesta del carrito...</p>
+      <div>{
+        loading
+        ?<Loading/>
+        :alertify.alert("Tu pago a sido Aprovado ", "GRacias por tu compra ")
+        }
+        
+        <Navbar/>
+        
       </div>
     );
-};
-
+  }
 export default RespuestaCarr;
