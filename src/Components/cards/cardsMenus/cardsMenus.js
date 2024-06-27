3df40323be6d7c359e1ff4e27227d3d1fdde58d5
fenixreds <114960950+fenixreds@users.noreply.github.com>
2024-06-27 18:38:33 -0500

    Actualizar menus por localstorage

--- src/Components/cards/cardsMenus/cardsMenus.js
+++ src/Components/cards/cardsMenus/cardsMenus.js
@@ -1,16 +1,56 @@
+// /* eslint-disable react/prop-types */
+// import { useSelector } from "react-redux";
+// import CardMenus from "../../card/cardMenus/cardMenus";
+// import "./cardsMenus.css";
+
+// function CardsMenus({ AllMenus, handleSelectMenu }) {
+
+//   const selectedRestaurant = useSelector((state)=>state.SELCTRESTAURANT)
+//   return (
+//     <div className="cardsMenusContainer">
+//   {AllMenus &&
+//     AllMenus.map((menu) =>
+//       selectedRestaurant === menu.restaurant_id ? (
+//         <CardMenus
+//           key={menu.id}
+//           id={menu.id}
+//           name={menu.name}
+//           restaurant_id={menu.restaurant_id}
+//           handleSelectMenu={handleSelectMenu}
+//         />
+//       ) : null
+//     )}
+// </div>
+//   );
+// }
+
+// export default CardsMenus;
+
 /* eslint-disable react/prop-types */
 import { useSelector } from "react-redux";
 import CardMenus from "../../card/cardMenus/cardMenus";
 import "./cardsMenus.css";
+import { getSelctRestaurantapp } from "../../Login/Login_Ingreso/LocalStorange_user/LocalStorange_user";
+import { useEffect, useState } from "react";
 
 function CardsMenus({ AllMenus, handleSelectMenu }) {
 
   const selectedRestaurant = useSelector((state)=>state.SELCTRESTAURANT)
+  
+  
+  const [restaurant, seletRestaurant] = useState()
+  useEffect(()=>{
+    if(!selectedRestaurant){
+      seletRestaurant(getSelctRestaurantapp())
+    }else{
+      seletRestaurant(selectedRestaurant)
+    }
+  },[selectedRestaurant])
   return (
     <div className="cardsMenusContainer">
   {AllMenus &&
     AllMenus.map((menu) =>
-      selectedRestaurant === menu.restaurant_id ? (
+      restaurant === menu.restaurant_id ? (
         <CardMenus
           key={menu.id}
           id={menu.id}
