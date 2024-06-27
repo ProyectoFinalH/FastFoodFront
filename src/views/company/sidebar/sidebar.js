0772e0cdb8aa00aa4ea88678fdebccb149909103
Daniel Navarro <danielkristopher0563@gmail.com>
2024-06-27 15:31:37 -0500

    ajuste css company

--- src/views/company/sidebar/sidebar.js
+++ src/views/company/sidebar/sidebar.js
@@ -1,56 +1,60 @@
-import { useState } from 'react';
+import { useState } from "react";
 import DetailCompany from "../detailCompany/detailCompany";
-import ProductsCompany from '../productsCompany/productsCompany';
-import OrderCompany from '../orderCompany/orderCompany';
-import "./sidebarCompany.css"
-import LoginPrincipal from '../../../Components/Login/Login_Principal/Login_Principal';
-import MenuesCompany from '../menuesCompany/menuesCompany';
-import CategoriesCompany from '../categoriesCompany/categoriesCompany';
-import CommentsCompany from '../comments/commentsCompany';
+import ProductsCompany from "../productsCompany/productsCompany";
+import OrderCompany from "../orderCompany/orderCompany";
+import "./sidebarCompany.css";
+import LoginPrincipal from "../../../Components/Login/Login_Principal/Login_Principal";
+import MenuesCompany from "../menuesCompany/menuesCompany";
+import CategoriesCompany from "../categoriesCompany/categoriesCompany";
+import CommentsCompany from "../comments/commentsCompany";
 
-function Sidebar({restaurant}) {
-
+function Sidebar({ restaurant }) {
   const [selectedOption, setSelectedOption] = useState(1);
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
   };
 
-
-
-
-
   return (
     <div className="sidebarContainer">
-      
-      <div className='OptionContainer'>
-      <ul>
-      <div className="cardRestConainerSidebar">
-        
-        <img src={restaurant?.image_url} alt="imglogRes"/>
-        
-        <div className="cardRestConainerSidebarh2">
-        <h2>{restaurant?.name}</h2>
-          </div>  
-      </div>
+      <div className="OptionContainer">
+        <ul>
+          <div className="cardRestConainerSidebar">
+            <img src={restaurant?.image_url} alt="imglogRes" />
 
-        <li tabindex="0" onClick={() => handleOptionClick(1)}>Productos</li>
-        <li tabindex="0" onClick={() => handleOptionClick(4)}>Menús</li>
-        <li tabindex="0" onClick={() => handleOptionClick(6)}>Categorías</li>
-        <li tabindex="0" onClick={() => handleOptionClick(2)}>Editar Perfíl</li>
+            <div className="cardRestConainerSidebarh2">
+              <h2>{restaurant?.name}</h2>
+            </div>
+          </div>
 
-        <li tabindex="0" onClick={() => handleOptionClick(3)}>Órdenes Realizadas</li>
-        <li tabindex="0" onClick={() => handleOptionClick(7)}>Comentarios/Caificaciones</li>
-        <li tabindex="0" onClick={() => handleOptionClick(5)}>Cerrar Sesión</li>
+          <li tabindex="0" onClick={() => handleOptionClick(1)}>
+            Productos
+          </li>
+          <li tabindex="0" onClick={() => handleOptionClick(4)}>
+            Menús
+          </li>
+          <li tabindex="0" onClick={() => handleOptionClick(6)}>
+            Categorías
+          </li>
+          <li tabindex="0" onClick={() => handleOptionClick(2)}>
+            Editar Perfíl
+          </li>
 
-      </ul>
-
+          <li tabindex="0" onClick={() => handleOptionClick(3)}>
+            Órdenes Realizadas
+          </li>
+          <li tabindex="0" onClick={() => handleOptionClick(7)}>
+            Comentarios/Caificaciones
+          </li>
+          <li tabindex="0" onClick={() => handleOptionClick(5)}>
+            Cerrar Sesión
+          </li>
+        </ul>
       </div>
       <div className="conent2">
-
         {selectedOption === 1 && <ProductsCompany />}
         {selectedOption === 4 && <MenuesCompany />}
-        {selectedOption === 2 && <DetailCompany/>}
+        {selectedOption === 2 && <DetailCompany />}
         {selectedOption === 3 && <OrderCompany />}
         {selectedOption === 5 && <LoginPrincipal />}
         {selectedOption === 6 && <CategoriesCompany />}
