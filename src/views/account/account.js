8995327e954c86a1a40958412ad34051f6be8733
Daniel Navarro <danielkristopher0563@gmail.com>
2024-06-27 15:58:50 -0500

    a

--- src/views/account/account.js
+++ src/views/account/account.js
@@ -1,4 +1,3 @@
-// src/Pages/Account/Account.js
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
