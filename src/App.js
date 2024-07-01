544241e0b96c0e8993d775706215ee6bf0683c42
Daniel Navarro <danielkristopher0563@gmail.com>
2024-07-01 17:43:06 -0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import "./tailwind.css";
 import LoginAdmin from "./Components/Login/Login_Admin/Login_Admin";
 import LoginPrincipal from "./Components/Login/Login_Principal/Login_Principal";
 import Account from "./views/account/account";
@@ -8,20 +8,15 @@ import Home from "./views/home/home";
 import Menu from "./views/menu/menu";
 import CreateMenuForm from "./Components/createMenu/createMenu";
 import Carrito from "./Components/Carrito/Carrito";
-import RespuestaCarr from "./Components/Carrito/Respuesta_Carrito"
-
-
+import RespuestaCarr from "./Components/Carrito/Respuesta_Carrito";
 import MisCompras from "./Components/MisCompras/MisCompras";
-
 import Company from "./views/company/company";
-
 import DetailCompany from "./views/company/detailCompany/detailCompany";
 import Admin from "./views/admin/admin";
-
 import { AuthProvider } from "./AuthContext/AuthContext";
 import ProtectedRoute from "./AuthContext/ProtectedRoute";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Dashboard from "./Dashboard/Dashboard"; // Verifica que esta ruta sea correcta
+import Dashboard from "./Dashboard/Dashboard";
 import ScrollToTopButton from "./Components/scrollTop/scrollTop";
 import OrderUsers from "./views/Orders_User/Order_User";
 import Maintenance from "./views/maintenance/maintenance";
@@ -30,58 +25,16 @@ import Loading from "./Components/loading/Loading";
 
 import Rating from "./Components/rating/rating";
 
-
-
-
-
 function App() {
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(false);
-  // const [dataLoaded, setDataLoaded] = useState(false);
-  // const [showApp, setShowApp] = useState(false);
-
-
-  // useEffect(() => {
-  //   const Data = async () => {
-  //     try {
-  //       const response = await axios.get("http://localhost:5000/menuitems");
-  //       if (response.data.length === 0) {
-  //         console.log("La tabla de restaurantes está vacía");
-  //       }
-  //       setDataLoaded(true);
-       
-  //     } catch (error) {
-  //       console.error("Error de red:", error);
-  //       setError(true);
-
-  //     }finally{
-  //       setLoading(false);
-  //       setShowApp(true);
-
-  //     }
-  //   };
-
-  //   Data();
-  // }, []);
-
-  // if (loading) {
-  //   return <Loading/> 
-  // }
-
-  // if (error) {
-  //   return <Maintenance />
-  // }
-
   return (
     <div className="App">
-       
       <AuthProvider>
         <Routes>
           <Route path="/LoginAdmin" element={<LoginAdmin />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/company" element={<Company />} />
           <Route path="/restaurants/:id" element={<DetailCompany />} />
-          <Route path="/reviews" element={<Rating/>}/>
+          <Route path="/reviews" element={<Rating />} />
           <Route path="/" element={<LoginPrincipal />} />
           <Route path="/home" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
@@ -90,7 +43,7 @@ function App() {
           <Route path="/menu/create" element={<CreateMenuForm />} />
           <Route path="/carrito" element={<Carrito />} />
           <Route path="/miscompras" element={<MisCompras />} />
-          <Route path="/userorders" element={<OrderUsers/>} /> {/*! Ordenes del usuario */}
+          <Route path="/userorders" element={<OrderUsers />} />{" "}
           <Route path="/respuestacarrito" element={<RespuestaCarr />} />
           <Route
             path="/dashboard"
@@ -98,9 +51,8 @@ function App() {
           />
           <Route path="*" element={<Navigate to="/home" />} />
         </Routes>
-      <ScrollToTopButton/>
+        <ScrollToTopButton />
       </AuthProvider>
-    
     </div>
   );
 }
